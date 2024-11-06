8d05f150ec67dcbd257f31eb1648669d8019adde
shakyra <shakyra.portes@gmail.com>
2024-11-06 13:53:37 -0300

    Deploy para GitHub Pages

--- cypress/e2e/example.cy.js
+++
@@ -1,8 +0,0 @@
-// https://on.cypress.io/api
-
-describe('Sanity Test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('#header a:first-child', 'Music')
-  })
-})
--- src/components/__tests__/about.spec.js
+++
@@ -1,9 +0,0 @@
-import AboutView from '@/views/AboutView.vue'
-import { shallowMount } from '@vue/test-utils'
-
-describe('AboutView.vue', () => {
-  test('renders inner text', () => {
-    const wrapper = shallowMount(AboutView)
-    expect(wrapper.text()).toContain('about')
-  })
-})
